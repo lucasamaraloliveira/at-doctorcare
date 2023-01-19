a3b128e2d76a450d6596a07eca4c5f62d79ed1a2
Lucas Amaral Oliveira da Silva <lucas.amaral.oliveira.silva@gmail.com>
2023-01-19 15:59:11 -0300

    first commit

---
+++ src/components/layout/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: none;
+    font-family: 'DM Sans', sans-serif;
+    list-style: none;
+  }
+  html, body {
+    background: var(--color-6);
+    height: 100vh;
+  }
+  :root {
+    --brand-blue: #0A4378;
+    --brand-blue-light: #1971C2;
+    --brand-blue-light-02: #D0EBFF;
+    --brand-blue-light-03: #F0F9FF;
+    --gray-headline: #061800;
+    --gray-paragraph: #495057;
+    --light-gray: #F9F9F9; 
+    --white: #FFFFFF;
+  }
+`;
